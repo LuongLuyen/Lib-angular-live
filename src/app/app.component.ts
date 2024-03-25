1355521db1114b9e32024ebebf4e56f135233e56
LuongLuyen <luongvanluyen2003@gmail.com>
2024-03-25 20:40:51 +0700

    component search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonW,Button1,Button2,Button3,Button4,Button5,Button6,Button7,Button8,Button9,Button10,Button11,Button12,Button13,Button14,Button15,Button16} from 'luongluyen/lib/btn'
-import {ComponentSearch,ComponentSearch1} from 'luongluyen/lib/search'
+import {ComponentSearch,ComponentSearch1,ComponentSearch2,ComponentSearch3} from 'luongluyen/lib/search'
+import {Card1} from 'luongluyen/lib/card'
 import { CommonModule } from '@angular/common';
 import { DomSanitizer } from '@angular/platform-browser';
 import 'luongluyen/index.css';
@@ -17,45 +18,44 @@ export class AppComponent {
   api: string ='http://localhost:5000/home-bong'
   title = 'TestLib';
   constructor(private sanitizer: DomSanitizer) { }
-  ButtonW: any;
-  Button1: any;
-  Button2: any;
-  Button3: any;
-  Button4: any;
-  Button5: any;
-  Button6: any;
-  Button7: any;
-  Button8: any;
-  Button9: any;
-  Button10: any;
-  Button11: any;
-  Button12: any;
-  Button13: any;
-  Button14: any;
-  Button15: any;
-  Button16: any;
-  ComponentSearch: any;
-  ComponentSearch1: any;
-  
+  buttons: any[]=[
+    {name:ButtonW,template:ButtonW("ButtonW")},
+    {name:Button1,template:Button1("Button1")},
+    {name:Button2,template:Button2("Button2")},
+    {name:Button3,template:Button3("Button3")},
+    {name:Button4,template:Button4("Button4")},
+    {name:Button5,template:Button5("Button5")},
+    {name:Button6,template:Button6("Button6")},
+    {name:Button7,template:Button7("Button7")},
+    {name:Button8,template:Button8("Button8")},
+    {name:Button9,template:Button9("Button9")},
+    {name:Button10,template:Button10("Button10")},
+    {name:Button11,template:Button11("Button11")},
+    {name:Button12,template:Button12("Button12")},
+    {name:Button13,template:Button13("Button13")},
+    {name:Button14,template:Button14("Button14")},
+    {name:Button15,template:Button15("Button15")},
+    {name:Button16,template:Button16("Button16")},
+  ]
+  componentSearchs:any[]=[
+    {name:ComponentSearch,template:ComponentSearch(this.api)},
+    {name:ComponentSearch1,template:ComponentSearch1(this.api)},
+    {name:ComponentSearch2,template:ComponentSearch2(this.api)},
+    {name:ComponentSearch3,template:ComponentSearch3(this.api)},
+  ]
+  cards:any[]=[
+    {name:Card1,template:Card1("")},
+  ]
+
   ngOnInit() {
-    this.ButtonW = this.sanitizer.bypassSecurityTrustHtml(ButtonW("ButtonW"));
-    this.Button1 = this.sanitizer.bypassSecurityTrustHtml(Button1("Button1"));
-    this.Button2 = this.sanitizer.bypassSecurityTrustHtml(Button2("Button2"));
-    this.Button3 = this.sanitizer.bypassSecurityTrustHtml(Button3("Button3"));
-    this.Button4 = this.sanitizer.bypassSecurityTrustHtml(Button4("Button4"));
-    this.Button5 = this.sanitizer.bypassSecurityTrustHtml(Button5("Button5"));
-    this.Button6 = this.sanitizer.bypassSecurityTrustHtml(Button6("Button6"));
-    this.Button7 = this.sanitizer.bypassSecurityTrustHtml(Button7("Button7"));
-    this.Button8 = this.sanitizer.bypassSecurityTrustHtml(Button8("Button8"));
-    this.Button9 = this.sanitizer.bypassSecurityTrustHtml(Button9("Button9"));
-    this.Button10 = this.sanitizer.bypassSecurityTrustHtml(Button10("Button10"));
-    this.Button11 = this.sanitizer.bypassSecurityTrustHtml(Button11("Button11"));
-    this.Button12 = this.sanitizer.bypassSecurityTrustHtml(Button12("Button12"));
-    this.Button13 = this.sanitizer.bypassSecurityTrustHtml(Button13("Button13"));
-    this.Button14= this.sanitizer.bypassSecurityTrustHtml(Button14("Button14"));
-    this.Button15= this.sanitizer.bypassSecurityTrustHtml(Button15("Button15"));
-    this.Button16= this.sanitizer.bypassSecurityTrustHtml(Button16("Button16"));
-    this.ComponentSearch = this.sanitizer.bypassSecurityTrustHtml(ComponentSearch(this.api));
-    this.ComponentSearch1 = this.sanitizer.bypassSecurityTrustHtml(ComponentSearch1(this.api));
+    this.buttons.forEach((button:any)=>{
+      button.name =this.sanitizer.bypassSecurityTrustHtml(button.template)
+    })
+    this.componentSearchs.forEach((search:any)=>{
+      search.name =this.sanitizer.bypassSecurityTrustHtml(search.template)
+    })
+    this.cards.forEach((card:any)=>{
+      card.name =this.sanitizer.bypassSecurityTrustHtml(card.template)
+    })
   }
 }
